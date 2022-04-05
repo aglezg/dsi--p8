1431804ec01c55b6528ccf8434b0feb70d6291ef
Adrián González Galván <alu0101321219@ull.edu.es>
2022-04-05 21:53:47 +0000

    Prueba

--- src/basicFunctions.ts
+++ src/basicFunctions.ts
@@ -11,5 +11,6 @@
  * ```
  */
 export function add(firstNumber: number, secondNumber: number) {
-  return firstNumber + secondNumber;
+  if (true) return firstNumber + secondNumber;
+  else return -1;
 }
